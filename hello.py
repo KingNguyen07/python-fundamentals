98dfa4f065e932cedb4b73241f89c6ff4755dbe8
James Nguyen <jnguyen43@wsutech.edu>
2022-03-01 18:32:45 -0600

    CAC-101 Created Python Project

---
+++ hello.py
@@ -0,0 +1,11 @@
+# This is a comment 'and you should have a space after the number sign.
+# Click the enter key after the comment to remove the squiggly yellow line.
+
+# This statement below is a top level statement.
+# It is defined outside a function and class.This specific statement
+# is a function called print that takes information and displays
+# that information to the terminal.
+print("Hello World")
+
+# A basic string can be defined using either single or double quotes.
+print('Hello Again')
