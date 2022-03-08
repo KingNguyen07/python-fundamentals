9e4870447a57dafac287af5c89be6c42913a563d
James Nguyen <jnguyen43@wsutech.edu>
2022-03-08 03:28:33 -0600

    CAC-101-2
    F

---
+++ exercises/exercise2.py
@@ -0,0 +1,55 @@
+# Lab Exercise-My Application Overview
+# 1. Write a function called simple(). Assign a different message to 5 variables
+# and print each message.
+def simple():
+    message = 1
+    print(message)
+
+    message = 2
+    print(message)
+
+    message = 3
+    print(message)
+
+    message = 4
+    print(message)
+
+    message = 5
+    print(message)
+
+
+simple()
+
+
+# Write a function simple 2(). Assign a message to a variable, then print out
+# that variable. Change the message and assign it to the variable again, but
+# after the 1st print statement. Print the 2nd message. Do these steps 2 more
+# times. You should have 4 messages assigned to the same variable and 4 print
+# functions showing the same results.
+
+
+def simple2():
+    message = 2
+    print(message)
+
+
+simple2()
+
+# Stuck On Example 2
+# I have no clue I spent 3 hours these instructions don't make sense
+# I dont get how I'm supposed to change the message without losing the print
+# variable.
+# As for right now im just turning in as is, I don't have enough time left
+# in the unit to dwell on one question for 3 hours,Moving on to All About Data
+
+
+def message(firstname,lastname):
+    """Print their last name first if given"""
+    print(lastname,firstname)
+
+
+message("James","Nguyen")
+
+
+
+
