5843d993496fc2bbd6026c44f8213182d350acce
James Nguyen <jnguyen43@wsutech.edu>
2022-03-15 00:49:43 -0500

    CAC-101-4

---
+++ exercises/exercise4.py
@@ -0,0 +1,56 @@
+# Lab Exercise - Numbers
+
+# Page 29
+# 2-8 Number Eight
+print(4+4)
+print(12-4)
+print(4*2)
+print(int(24/3))
+
+# 2-9 Favorite Number
+Favorite_Number = 7
+message1 = 'My favorite number is ' + str(Favorite_Number)
+print(message1)
+
+# Number 2
+
+
+def number_sets():
+    number_one = 456_234
+    number_two = 68_423_791
+    number_three = 13_794_628
+    number_four = 96_374
+
+    print(number_one, number_two, number_three, number_four)
+
+
+number_sets()
+
+# Number 3
+
+
+def function1():
+    """Print both arguments """
+    arg1 = float(4)
+    arg2 = int(3.0)
+    print(arg1, arg2)
+
+
+function1()
+
+
+# Number 4
+
+
+def function2():
+    movie = input('What is your favorite movie?')
+    views = input('How many times have you seen it?')
+
+    print('I have seen '+str(movie)+' '+str(views)+' times')
+
+
+function2()
+
+
+
+
